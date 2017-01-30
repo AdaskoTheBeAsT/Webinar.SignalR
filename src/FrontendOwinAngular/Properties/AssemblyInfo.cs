0cae404a139909f444b866cb0f7d94e25924495e
Adam Pluciński <adaskothebeast@gmail.com>
2017-01-30 23:27:18 +0100

    added angular frontend stub

---
+++ src/FrontendOwinAngular/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("FrontendOwinAngular")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FrontendOwinAngular")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
